f7db8a347e965a84e6401645591db04719db603c
xzadudu179 <terra179@163.com>
2024-10-30 03:48:50 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Hello from './components/Hello.vue'
+</script>
+
+<template>
+  <main>
+    <div class="card">
+          <div class="avatar">
+          <img alt="xzadudu179" class="logo" src="./assets/img/avatar.png" height="525" />
+        </div>
+        <article>
+          <Hello/>
+        </article>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+/* .logo {
+  filter: drop-shadow(0 0 10px var(--primary-color));
+} */
+.card {
+  width: 1200px;
+}
+</style>
---
+++ src/components/InfoItem.vue
@@ -0,0 +1,3 @@
+<template>
+    <!-- TODO: 做出标题 内容并且可修改 -->
+</template>
